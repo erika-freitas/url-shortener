9d2eb72bc34efef5159f01cbe80d788a0f959df3
Erika Freitas <erikamsfreitas@gmail.com>
2022-07-17 22:38:19 -0300

    Initial project configuration

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { URLController } from './controller/URLController';
+import express, { Request, Response } from 'express';
+import { MongoConnection } from './database/MongoConnection';
+
+const api = express();
+api.use(express.json());
+
+const database = new MongoConnection()
+database.connect()
+
+const urlController = new URLController();
+api.post('/shorten', urlController.shorten)
+api.get('/:hash', urlController.redirect)
+
+api.listen(5000, () => {
+  console.log('Api running on port 5000');
+});
